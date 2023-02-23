8045b66f040bfc33d8ec3c7bbc20b3cd4f1e5089
gokberkkeptig <gokberk.keptig@gmail.com>
2023-02-23 15:21:18 +0100

    Added models

---
+++ models/DCGAN_Discriminator_1.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 64 x 32 x 32
+
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 128 x 16 x 16
+
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 256 x 8 x 8
+
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 512 x 4 x 4
+
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Flatten(),
+            nn.Sigmoid()
+            # 1 x 1 x 1
+        )
+
+    def forward(self, input):
+        return self.main(input)
---
+++ models/DCGAN_Generator_0.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+
+class DCGAN_Generator_Model_0(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.main = nn.Sequential(
+            #input 100 * 1024 * 3
+
+            nn.ConvTranspose2d(100, 1024,kernel_size= 4, stride= 2, padding=1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(True),
+            #output  3 * 3
+
+            nn.ConvTranspose2d(1024, 512, kernel_size= 4, stride=2,padding= 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            #output  7*7
+
+            nn.ConvTranspose2d(512, 256, kernel_size= 4, stride=2,padding= 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            #output  14 * 14
+
+            nn.ConvTranspose2d(256, 128, kernel_size= 4, stride=2,padding= 1,bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # output  28*28
+
+            nn.ConvTranspose2d(128, 64, kernel_size= 4, stride=2,padding= 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(64, 3, kernel_size= 4, stride=2,padding= 1, bias=False),
+            nn.Tanh()
+            )
+
+    def forward(self, input):
+        return self.main(input)
